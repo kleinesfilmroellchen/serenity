dcbd2b31a426a2682c185f1aeffe495b0022bc3c
David Isaksson <davidisaksson93@gmail.com>
2021-09-28 18:08:39 +0200

    LibAudio: Move  conversion utility functions out of Audio::Samples
    So we can access these outside of the class scope

--- Userland/Libraries/LibAudio/Sample.h
+++ Userland/Libraries/LibAudio/Sample.h
@@ -12,12 +12,48 @@
 
 namespace Audio {
 using namespace AK::Exponentials;
-// Constants for logarithmic volume. See Sample::linear_to_log
-// Corresponds to 60dB
+
+// Logarithmic scaling, as audio should ALWAYS do.
+// Reference: https://www.dr-lex.be/info-stuff/volumecontrols.html
+// We use the curve `factor = a * exp(b * change)`,
+// where change is the input fraction we want to change by,
+// a = 1/1000, b = ln(1000) = 6.908 and factor is the multiplier used.
+// The value 1000 represents the dynamic range in sound pressure, which corresponds to 60 dB(A).
+// This is a good dynamic range because it can represent all loudness values from
+// 30 dB(A) (barely hearable with background noise)
+// to 90 dB(A) (almost too loud to hear and about the reasonable limit of actual sound equipment).
+//
+// Format ranges:
+// - Linear:        0.0 to 1.0
+// - Logarithmic:   0.0 to 1.0
+// - dB:          -60.0 to 0.0
+
 constexpr double DYNAMIC_RANGE = 1000;
 constexpr double DYNAMIC_RANGE_DB = 60;
 constexpr double VOLUME_A = 1 / DYNAMIC_RANGE;
 double const VOLUME_B = log(DYNAMIC_RANGE);
+
+ALWAYS_INLINE double linear_to_log(double const change)
+{
+    // TODO: Add linear slope around 0
+    return VOLUME_A * exp(VOLUME_B * change);
+}
+
+ALWAYS_INLINE double log_to_linear(double const val)
+{
+    // TODO: Add linear slope around 0
+    return log(val / VOLUME_A) / VOLUME_B;
+}
+
+ALWAYS_INLINE double db_to_linear(double const dB)
+{
+    return (dB + DYNAMIC_RANGE_DB) / DYNAMIC_RANGE_DB;
+}
+
+ALWAYS_INLINE double linear_to_db(double const val)
+{
+    return val * DYNAMIC_RANGE_DB - DYNAMIC_RANGE_DB;
+}
 
 // A single sample in an audio buffer.
 // Values are floating point, and should range from -1.0 to +1.0
@@ -49,43 +85,6 @@ struct Sample {
             right = 1;
         else if (right < -1)
             right = -1;
-    }
-
-    // Logarithmic scaling, as audio should ALWAYS do.
-    // Reference: https://www.dr-lex.be/info-stuff/volumecontrols.html
-    // We use the curve `factor = a * exp(b * change)`,
-    // where change is the input fraction we want to change by,
-    // a = 1/1000, b = ln(1000) = 6.908 and factor is the multiplier used.
-    // The value 1000 represents the dynamic range in sound pressure, which corresponds to 60 dB(A).
-    // This is a good dynamic range because it can represent all loudness values from
-    // 30 dB(A) (barely hearable with background noise)
-    // to 90 dB(A) (almost too loud to hear and about the reasonable limit of actual sound equipment).
-    //
-    // Format ranges:
-    // - Linear:        0.0 to 1.0
-    // - Logarithmic:   0.0 to 1.0
-    // - dB:          -60.0 to 0.0
-
-    ALWAYS_INLINE double linear_to_log(double const change)
-    {
-        // TODO: Add linear slope around 0
-        return VOLUME_A * exp(VOLUME_B * change);
-    }
-
-    ALWAYS_INLINE double log_to_linear(double const val)
-    {
-        // TODO: Add linear slope around 0
-        return log(val / VOLUME_A) / VOLUME_B;
-    }
-
-    ALWAYS_INLINE double db_to_linear(double const dB)
-    {
-        return (dB + DYNAMIC_RANGE_DB) / DYNAMIC_RANGE_DB;
-    }
-
-    ALWAYS_INLINE double linear_to_db(double const val)
-    {
-        return val * DYNAMIC_RANGE_DB - DYNAMIC_RANGE_DB;
     }
 
     ALWAYS_INLINE Sample& log_multiply(double const change)
