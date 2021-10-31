dbbce212575f02df60c9f2446b471d7e9ddcb9e7
kleines Filmröllchen <malu.bertsch@gmail.com>
2021-10-31 15:43:49 +0100

    Demos/BadApple: Play the video primitively
    The performance is bad, but hey, it plays Bad Apple!

--- Userland/Demos/BadApple/BadApple.cpp
+++ Userland/Demos/BadApple/BadApple.cpp
@@ -4,6 +4,9 @@
  * SPDX-License-Identifier: BSD-2-Clause
  */
 
+#include "AK/NonnullRefPtr.h"
+#include "LibGfx/Forward.h"
+#include "LibGfx/Painter.h"
 #include <AK/Format.h>
 #include <AK/RefPtr.h>
 #include <LibCore/ElapsedTimer.h>
@@ -27,6 +30,8 @@
 #include <unistd.h>
 
 constexpr int FRAME_RATE = 20;
+String const BASE_PATH = "/res/badapple";
+String const FRAMES_FOLDER = "frames";
 
 class BadApple : public GUI::Frame {
     C_OBJECT(BadApple);
@@ -38,6 +43,9 @@ private:
     BadApple();
     virtual void paint_event(GUI::PaintEvent&) override;
     virtual void timer_event(Core::TimerEvent&) override;
+
+    RefPtr<Gfx::Bitmap> frame;
+    unsigned framenum { 1 };
 };
 
 BadApple::BadApple()
@@ -47,16 +55,26 @@ BadApple::BadApple()
     start_timer(1000. / static_cast<double>(FRAME_RATE));
 }
 
-void BadApple::paint_event(GUI::PaintEvent& evt)
+void BadApple::paint_event(GUI::PaintEvent& event)
 {
-    (void)evt;
-    dbgln("painting bad apple");
+    if (frame.is_null())
+        return;
+    GUI::Painter painter(*this);
+    painter.add_clip_rect(event.rect());
+    painter.blit(Gfx::IntPoint(0, 0), *frame, frame->rect());
 }
 
 void BadApple::timer_event(Core::TimerEvent& evt)
 {
     (void)evt;
-    dbgln("frame timer bad apple");
+
+    ++framenum;
+    frame = Gfx::Bitmap::try_load_from_file(String::formatted("{}/{}/{:04d}.png", BASE_PATH, FRAMES_FOLDER, framenum));
+    if (frame.is_null()) {
+        warnln("Couldn't load frame {}", framenum);
+        return;
+    }
+    update();
 }
 
 int main(int argc, char** argv)
@@ -80,13 +98,21 @@ int main(int argc, char** argv)
 
     NonnullRefPtr<GUI::Window> window = GUI::Window::construct();
 
-    BadApple& badapple = window->set_main_widget<BadApple>();
-    (void)badapple;
+    auto app_icon = GUI::Icon::default_icon("app-badapple");
+    window->set_icon(app_icon.bitmap_for_size(16));
 
+    // this order is important
+    window->set_double_buffering_enabled(false);
+    window->set_title("Bad Apple!!");
+    window->set_resizable(false);
+    window->resize(960, 720);
     window->show();
     window->set_minimum_size(960, 720);
     window->set_maximized(false);
     window->center_on_screen();
+
+    BadApple& badapple = window->set_main_widget<BadApple>();
+    (void)badapple;
 
     return app->exec();
 }
