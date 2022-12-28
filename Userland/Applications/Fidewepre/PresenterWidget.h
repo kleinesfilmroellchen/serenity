53ee4e5db851f5754b87bc6f335f857909bd4907
kleines Filmröllchen <filmroellchen@serenityos.org>
2022-12-29 00:17:16 +0100

    Presenter: Implement per-frame PNG export
    This export is not very configurable yet, but already runs on a
    background thread with rich status updates. The picked png file is used
    as the base name for the specific slide and frame PNG exports.

--- Userland/Applications/Fidewepre/PresenterWidget.h
+++ Userland/Applications/Fidewepre/PresenterWidget.h
@@ -10,12 +10,27 @@
 #include <Kernel/API/KeyCode.h>
 #include <LibGUI/Action.h>
 #include <LibGUI/Event.h>
+#include <LibGUI/Progressbar.h>
 #include <LibGUI/SettingsWindow.h>
 #include <LibGUI/UIDimensions.h>
 #include <LibGUI/Widget.h>
+#include <LibThreading/BackgroundAction.h>
+#include <LibThreading/ConditionVariable.h>
 
 // Title, Author
 constexpr StringView const title_template = "{} ({}) — Presenter"sv;
+
+enum class SlideProgress {
+    Starting,
+    Rendering,
+    Writing,
+    Error,
+};
+
+struct ExportState {
+    unsigned current_slide { 0 };
+    SlideProgress progress { SlideProgress::Starting };
+};
 
 class PresenterWidget : public GUI::Widget {
     C_OBJECT(PresenterWidget);
@@ -37,6 +52,8 @@ protected:
 
 private:
     void go_to_slide_from_key_sequence();
+    void on_export_slides_action();
+    void update_export_status();
 
     OwnPtr<Presentation> m_current_presentation;
     RefPtr<GUI::Action> m_next_slide_action;
@@ -44,4 +61,10 @@ private:
     RefPtr<GUI::SettingsWindow> m_settings_window;
 
     Vector<KeyCode, 3> m_current_key_sequence;
+    RefPtr<Threading::BackgroundAction<int>> m_exporter;
+    Threading::MutexProtected<ExportState> m_export_state {};
+    Threading::ConditionVariable m_export_state_updated { m_export_state };
+    Atomic<bool> m_export_done { false };
+    RefPtr<GUI::Window> m_progress_window {};
+    RefPtr<GUI::Progressbar> m_progress_bar;
 };
