b59f973dd6bba2480b17874dc4d50f6e48ad7632
kleines Filmröllchen <filmroellchen@serenityos.org>
2023-06-02 17:17:46 +0200

    Fidewepre: Pledge FileManager config domain
    This is a workaround until the situation is properly resolved upstream.

--- Userland/Applications/Fidewepre/main.cpp
+++ Userland/Applications/Fidewepre/main.cpp
@@ -26,7 +26,7 @@ ErrorOr<int> serenity_main(Main::Arguments arguments)
     auto app = TRY(GUI::Application::create(arguments));
     auto window = TRY(GUI::Window::try_create());
 
-    Config::pledge_domain("Presenter");
+    Config::pledge_domains({ "Presenter", "FileManager" });
 
     window->set_title("Presenter");
     window->set_icon(GUI::Icon::default_icon("app-presenter"sv).bitmap_for_size(16));
