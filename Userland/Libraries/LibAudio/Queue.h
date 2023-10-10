4ba9757d5eccd1f1672cd51f08e61ca4541e69c6
kleines Filmröllchen <filmroellchen@serenityos.org>
2023-10-10 18:57:17 +0200

    LibAudio: Adjust queue parameters
    Use a slighly larger audio buffer size that's a power of two, and halve
    the number of buffers. These don't seem to affect audio stuttering.

--- Userland/Libraries/LibAudio/Queue.h
+++ Userland/Libraries/LibAudio/Queue.h
@@ -11,11 +11,11 @@
 
 namespace Audio {
 
-static constexpr size_t AUDIO_BUFFERS_COUNT = 128;
+static constexpr size_t AUDIO_BUFFERS_COUNT = 64;
 // The audio buffer size is specifically chosen to be about 1/1000th of a second (1ms).
 // This has the biggest impact on latency and performance.
 // The currently chosen value was not put here with much thought and a better choice is surely possible.
-static constexpr size_t AUDIO_BUFFER_SIZE = 50;
+static constexpr size_t AUDIO_BUFFER_SIZE = 64;
 using AudioQueue = Core::SharedSingleProducerCircularQueue<Array<Sample, AUDIO_BUFFER_SIZE>, AUDIO_BUFFERS_COUNT>;
 
 }
