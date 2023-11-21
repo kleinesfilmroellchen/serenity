5787540dea55262fe8203c92bb7bd45daf232898
kleines Filmröllchen <filmroellchen@serenityos.org>
2023-11-21 20:03:06 +0100

    shutdown: Use SystemServer for shutdown

--- Userland/Utilities/shutdown.cpp
+++ Userland/Utilities/shutdown.cpp
@@ -7,19 +7,38 @@
  */
 
 #include <AK/ByteString.h>
+#include <LibCore/EventLoop.h>
 #include <LibCore/File.h>
 #include <LibMain/Main.h>
+#include <SystemServer/ConnectionToServer.h>
 #include <fcntl.h>
 #include <stdio.h>
 #include <unistd.h>
 
+static ErrorOr<void> try_system_server_shutdown()
+{
+    auto server_connection = TRY(SystemServer::ControlClient::connect_to_init_system_server());
+    auto result = server_connection->activate_target("shutdown"_string);
+    if (result.has_value())
+        return ENOENT;
+    return {};
+}
+
 ErrorOr<int> serenity_main(Main::Arguments)
 {
-    auto file = TRY(Core::File::open("/sys/kernel/power_state"sv, Core::File::OpenMode::Write));
+    Core::EventLoop loop;
 
-    ByteString const file_contents = "2";
-    TRY(file->write_until_depleted(file_contents.bytes()));
-    file->close();
+    auto result = try_system_server_shutdown();
+
+    if (result.is_error()) {
+        outln("warning: can’t shutdown via SystemServer, performing a hard kernel shutdown instead.");
+
+        auto file = TRY(Core::File::open("/sys/kernel/power_state"sv, Core::File::OpenMode::Write));
+
+        ByteString const file_contents = "2";
+        TRY(file->write_until_depleted(file_contents.bytes()));
+        file->close();
+    }
 
     return 0;
 }
